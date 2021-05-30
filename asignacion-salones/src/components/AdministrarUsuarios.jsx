f7fc84d6e7984ba8bfb3c9bcb5e137b7283d3182
JAlexanderVelasquez <jhojan-alexander@hotmail.com>
2021-05-29 23:36:33 -0500

    Edit users from Admin view

--- asignacion-salones/src/components/AdministrarUsuarios.jsx
+++ asignacion-salones/src/components/AdministrarUsuarios.jsx
@@ -4,20 +4,23 @@ import { firedb } from "../firebaseconfig";
 const AdministrarUsuarios = () => {
 
     const [listaUsuario, setListaUsuario] = useState([]);
+    const [idUsuario, setIdUsuario] = useState("");
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [msgError, setMsgError] = useState(null);
+    const [cambioUsuarios, setCambioUsuarios] = useState(false);
 
     useEffect(() => {
-        const getUsuarios = async () => {
-            const { docs } = await firedb.collection('Usuario').get()
-            const nuevaLista = docs?.map( item => ({id: item.id, ...item.data()}))
-            console.log(nuevaLista)
-            setListaUsuario(nuevaLista)
-        }
         getUsuarios();
-    }, [listaUsuario]);
+    }, []);
+
+    const getUsuarios = async () => {
+        const { docs } = await firedb.collection('Usuario').get()
+        const nuevaLista = docs?.map( item => ({id: item.id, ...item.data()}))
+        console.log(nuevaLista)
+        setListaUsuario(nuevaLista)
+    }
 
     const setUsuarios = async (e) => {
         e.preventDefault();
@@ -33,20 +36,20 @@ const AdministrarUsuarios = () => {
             setMsgError("El campo password  esta vacio")
             return
         }
-
-        const newUser = {
+        const usuarioMod = await firedb.collection("Usuario").doc(idUsuario);
+        return usuarioMod.update({
             name: name,
             email: email,
             password: password,
-            active: true,
-        }
-        console.log("USER TO STORE,",newUser)
-        try {
-            //const data = await firedb.collection('usuario').add(newUser)
-            console.log("Data stored: ", "data")
-        } catch (error) {
-            console.log("Store error: ",error)
-        }
+        })
+        .then(() => {
+            alert("Usuario Actualizado correctamente")
+            Limpiardatos(e);
+            getUsuarios();
+        })
+        .catch((error) => {
+            console.error("Error updating document: ", error);
+        });
     }
 
     const inhabilitarUsuario = async(id) => {
@@ -56,6 +59,7 @@ const AdministrarUsuarios = () => {
         })
         .then(() => {
             alert("Usuario inhabilitado correctamente")
+            getUsuarios();
         })
         .catch((error) => {
             console.error("Error updating document: ", error);
@@ -68,84 +72,38 @@ const AdministrarUsuarios = () => {
         })
         .then(() => {
             alert("Usuario inhabilitado correctamente")
+            getUsuarios();
         })
         .catch((error) => {
             console.error("Error updating document: ", error);
         });
     }
 
-    const ActualizarUsuario = (e) => {
-        //e.preventDefault()
-        /*        if(name !== location.state?.name){
-                    user.updateProfile({
-                    displayName: name,
-                    }).then(() => {
-                        alert("Nombre actualizado correctamente");
-                    }).catch((error) => {
-                        alert("Error al actualizar el nombre")
-                        console.log("nombreError",error)
-                    });
-                }
-                if(email !== location.state?.email){
-                    user.updateEmail(email).then(() => {
-                        alert("Email actualizado correctamente");
-                      }).catch((error) => {
-                        alert("Error al actualizar el email")
-                        console.log("emailError",error)
-                      });
-                }
-                if(password !== location.state?.password){
-                    if(!password.trim()){
-                        alert('Debe ingresar una contraseña')
-                        return
-                    }
-                    if(password !== password2){
-                        alert('Las contraseñas deben ser iguales')
-                        return
-                    }
-                    user.updatePassword(password).then(() => {
-                        alert("Contraseña actualizada correctamente");
-                      }).catch((error) => {
-                        alert("Error al actualizar la contraseña")
-                        console.log("contraseñaError",error)
-                      });
-                }
-                setMsgError(null)
+    const editarUsuario = async (id) => {
+        try {
+            const data = await firedb.collection('Usuario').doc(id).get()
+            const { name, email, password } = data.data()
+            setName(name)
+            setEmail(email)
+            setPassword(password)
+            setIdUsuario(id)
             
-                window.location.reload(false);
-                alert("Actualizacion exitosa")
-          */
-    };
-
-    const getUsuarios = () => {
-        /*    admin.auth()
-                .getUsers([ ])
-                .then((getUsersResult) => {
-                    console.log('Successfully fetched user data:');
-                    getUsersResult.users.forEach((userRecord) => {
-                        console.log(userRecord);
-                    });
-                    console.log('Unable to find users corresponding to these identifiers:');
-                    getUsersResult.notFound.forEach((userIdentifier) => {
-                        console.log(userIdentifier);
-                    });
-                    })
-                .catch((error) => {
-                    console.log('Error fetching user data:', error);
-                });*/
-    };
-    /*<button onClick={getUsuarios} className="btn btn-danger float-right">
-                  BuscarUSUARIOS
-        </button>*/
+        } catch (error) {
+            console.log("ERROR:",error)
+        }
+    }
+    const Limpiardatos = (e) => {
+        e.preventDefault()
+        setName("")
+        setEmail("")
+        setPassword("")
+    }
     return (
         <div className="container">
             <div className="row mt-5">
                 <div className="col-4">
                     <h2 className="text-center">Administrar Usuarios</h2>
-                    <form
-                        onSubmit={setUsuarios}
-                        className="form-group text-center mt-5"
-                    >
+                    <form className="form-group text-center mt-5">
                         <input
                             onChange={(e) => {
                                 setName(e.target.value);
@@ -176,11 +134,17 @@ const AdministrarUsuarios = () => {
                             value={password}
                             type="password"
                         />
-                        <input
-                            className="btn btn-dark btn-block mt-4"
-                            value="Actualizar Datos"
-                            type="submit"
-                        />
+                        <button
+                            className="btn btn-dark btn-block m-4"
+                            onClick={(e) => {setUsuarios(e)}} 
+                        >Actualizar Datos
+                        </button> 
+                        <button
+                            onClick={(e) => {Limpiardatos(e)}} 
+                            className="btn btn-danger btn-block m-2">
+                                Limpiar Datos
+                        </button>
+
                     </form>
                     {msgError != null ? (
                         <div className="alert alert-danger mt-4">{msgError}</div>
@@ -199,7 +163,7 @@ const AdministrarUsuarios = () => {
                                 listaUsuario?.map(item => (
                                     <li className="list-group-item d-flex justify-content-between row" key={item.id}> 
                                     <p className={item.active? "col-8 text-justify":"col-8 text-justify text-secondary"} >{item.name} - {item.email} </p>
-                                    <button  className="btn btn-secondary col mx-2">Editar</button>
+                                    <button onClick={(id) => {editarUsuario(item.id) }} className="btn btn-secondary col mx-2">Editar</button>
                                     {
                                         item.active ?
                                         <button onClick={(id) => {inhabilitarUsuario(item.id)}} className="btn btn-danger col mx-2">Deshabilitar</button> 
