02c42d52ae716429e3b594dc325164572850a9ec
JAlexanderVelasquez <jhojan-alexander@hotmail.com>
2021-06-06 14:07:57 -0500

    Upload files with fire Storage

--- asignacion-salones/src/components/CargarArchivos.jsx
+++ asignacion-salones/src/components/CargarArchivos.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { store } from '../firebaseconfig'
 
 const CargarArchivos = () => {
+
+    const [date, setDate] = useState(new Date().toLocaleDateString())
+    const [progreso, setProgreso] = useState(0)
+    const [archivoD, setArchivoD] = useState("")
+    const [archivoA, setArchivoA] = useState("")
+
+    useEffect(() => {
+        setDate(new Date().toLocaleDateString().replaceAll("/","-"));
+        console.log(date);
+    }, [])
+    //const dat = new Date().toISOString();
+
+    const subirArchivos = () => {
+        if(archivoD == null || archivoD === undefined){
+            alert("Por favor seleccione un archivo para la disponibilidad docente")
+            return
+        }
+        if(archivoA == null || archivoA === undefined){
+            alert("Por favor seleccione un archivo para la programacion academica")
+            return
+        }
+//        const sotrageRef = store.ref(`Historial/${date}/${archivoD.name}`)
+        const sotrageRef = store.ref(`Historial/${date}/Disponibilidad`)
+        const task = sotrageRef.put(archivoD);
+        task.on('state_changed',
+            (snapshot) => {
+                setProgreso((snapshot.bytesTransferred / snapshot.totalBytes)*100);
+                console.log(progreso)
+            },
+            function error(err) {
+                console.log(err);
+            },
+            function complete() {   
+                console.log("FINALIZADO DDD")
+                alert(`Archivo ${archivoD.name} subido con exito`)
+            }
+        )
+        //const sotrageRefA = store.ref(`Historial/${date}/${archivoA.name}`)
+        const sotrageRefA = store.ref(`Historial/${date}/Programacion`)
+        const taskA = sotrageRefA.put(archivoA);
+        taskA.on('state_changed',
+            (snapshot) => {
+                setProgreso((snapshot.bytesTransferred / snapshot.totalBytes)*100);
+            },
+            function error(err) {
+                console.log(err);
+            },
+            function complete() {
+                console.log("FINALIZADO AAA")
+                alert(`Archivo ${archivoA.name} subido con exito`)
+            }
+        )
+    }
     return (
         <div className="container">
-            <h2>Cargue los archivos</h2>
+            <h2 className=" my-5">Cargue los archivos</h2>
+            
+            <form className="form-group text-center">
+                <p className=" my-4">Seleccion la disponibilidad docente</p>
+                <input className="form-control my-4" onChange={(e) => { setArchivoD(e.target.files[0]) }} type="file" accept=".csv" />
+                <p className=" my-4">Seleccion la programacion academica</p>
+                <input className="form-control my-4" onChange={(e) => { setArchivoA(e.target.files[0]) }} type="file" accept=".csv" />
+                <button className="btn btn-block btn-primary mt-4" onClick={(e) => {e.preventDefault(); subirArchivos();  }}>Enviar Archivos</button>
+            </form>
         </div>
     )
 }
--- asignacion-salones/src/firebaseconfig.js
+++ asignacion-salones/src/firebaseconfig.js
@@ -1,11 +1,13 @@
 import firebase from 'firebase/app'//En caso de presentar fallos auth eliminar /app
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage'
 
 const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
   // Initialize Firebase
   const fire = firebase.initializeApp(firebaseConfig);
   const auth = fire.auth();
-  const firedb = fire.firestore()
+  const firedb = fire.firestore();
+  const store = fire.storage();
 
-  export { auth, firedb }
+  export { auth, firedb, store }
