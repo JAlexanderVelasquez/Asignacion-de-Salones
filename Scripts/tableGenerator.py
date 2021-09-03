1f0917db6c089972a2af416412330724b5fa26eb
JAlexanderVelasquez <jhojan-alexander@hotmail.com>
2021-09-02 23:22:32 -0500

    tableGenerator modified

--- Scripts/tableGenerator.py
+++ Scripts/tableGenerator.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 mat = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -27,7 +29,7 @@ def obtenerHoras(posI,posJ,matrix):
     dia = dias[posI // 16]
     horaInicio = posI % 16 + 6
     horaInicio = str(horaInicio) + ":00" if (horaInicio < 18) else  str(horaInicio) + ":20"
-    salon = posJ + 1
+    salon = "Sala de sistemas " + str(posJ % 23 + 1)  if (posJ % 23 == 0 or posJ == 1) else (posJ + 1) % 17 
     while (matrix[posI][posJ] == num and num != 0):
         posI += 1
         if(posI == 96):
@@ -35,7 +37,8 @@ def obtenerHoras(posI,posJ,matrix):
             break
     horaFin = posI % 16 + 6
     horaFin = str(horaFin) + ":20" if (horaFin > 18) else  str(horaFin) + ":00"
-    return (num,dia,horaInicio,horaFin,salon)
+    sede = "Principe" if(posJ < 17) else "Villa campestre"
+    return (num,dia,horaInicio,horaFin,salon,sede)
 #print(obtenerHoras(3,7,mat))
 #print(obtenerHoras(89,17,mat))
 #print(obtenerHoras(92,17,mat))
@@ -68,10 +71,36 @@ def getAsignatures():
     for i in range(len(strMatrix)):
         for j in range(len(strMatrix[i])):
             if(strMatrix[i][j]!=0):
-                if(not inTupleArray(materiasHoras,strMatrix[i][j])):
+                #if(not inTupleArray(materiasHoras,strMatrix[i][j])):
+                if(i==0 or strMatrix[i][j] != strMatrix[i-1][j]):
                     materiasHoras.append(obtenerHoras(i,j,strMatrix))
     print(materiasHoras)
-    return materiasHoras
-
-
+    #return materiasHoras
+    progCsv=pd.read_csv("prog.csv",sep=";",encoding='latin-1')
+    data = progCsv.copy()
+    data["SEDE"] = None
+    data["DIA"] = None
+    data["ENTRADA"] = None
+    data["SALIDA"] = None
+    print(data)
+    #print(data.iloc[2]["CODIGO"])
+    pos = 0
+    for i in materiasHoras:
+        if(data.at[i[0]-1,"DIA"] != None):
+            pos = len(data.index)
+            data.loc[pos] = data.iloc[i[0]-1]
+            data.at[i[0]-1,"SALON"] =i[4]
+            data.at[i[0]-1,"DIA"] = i[1]
+            data.at[i[0]-1,"ENTRADA"] = i[2]
+            data.at[i[0]-1,"SALIDA"] = i[3]
+            data.at[i[0]-1,"SEDE"] = i[5]
+            #print("duplicated",i[0])
+        data.at[i[0]-1,"SALON"] =i[4]
+        data.at[i[0]-1,"DIA"] = i[1]
+        data.at[i[0]-1,"ENTRADA"] = i[2]
+        data.at[i[0]-1,"SALIDA"] = i[3]
+        data.at[i[0]-1,"SEDE"] = i[5]
+    print(data)
+    data.to_csv('asignacion.csv', index=False ,sep=";",mode='w')
+    
 getAsignatures()
